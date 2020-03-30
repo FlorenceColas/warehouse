84b266452739a18bd700546f789122db334be752
Florence Colas <colasflorence8@gmail.com>
2020-03-30 15:51:32 +0200

    bugfix form post action

--- module/Common/config/ConfigProvider.php
+++ module/Common/config/ConfigProvider.php
@@ -5,14 +5,8 @@ namespace Common;
 return [
     'service_manager' => [
         'factories' => [
-//            Helper\Translate::class         => Helper\TranslateFactory::class,
-//            Helper\TranslatePlural::class   => Helper\TranslatePluralFactory::class,
             PdfAdapter::class               => PdfAdapterFactory::class,
             PdfRenderer::class              => PdfRendererFactory::class,
-        ],
-        'invokables' => [
-//            Helper\DateFormat::class        => Helper\DateFormat::class,
-//            Helper\NumberFormat::class      => Helper\NumberFormat::class,
         ],
     ],
 ];
